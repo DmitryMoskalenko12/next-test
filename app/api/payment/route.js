101f36817154dad8e8f5c924a759f881b4a5711e
Dima Moskalenko <dmtrmsklnk@gmail.com>
2024-09-10 14:44:44 +0300

    test

---
+++ app/api/payment/route.js
@@ -0,0 +1,21 @@
+import { redirect } from "next/navigation";
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+      const data = req.body;
+  
+      // Проверка статуса платежа
+      if (data.status === 'Approved') {
+         redirect('/success') 
+        // Можно обновить статус заказа в базе данных
+      } else {
+        redirect('/failed') 
+      }
+  
+      // Ответ WayForPay
+      res.status(200).json({ status: 'success' });
+    } else {
+      // Если метод не POST
+      res.status(405).json({ message: 'Method Not Allowed' });
+    }
+  }
